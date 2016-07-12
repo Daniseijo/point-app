196afd931d3ff49831330aba57be94363161bf9d
Daniel Seijo Sánchez <daniseijo12@gmail.com>
2016-07-12 17:05:17 +0200

    Initial commit Xcode Project

---
+++ PointApp/PointApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PointApp
+//
+//  Created by Daniel Seijo Sánchez on 12/07/16.
+//  Copyright © 2016 Daniel Seijo Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
